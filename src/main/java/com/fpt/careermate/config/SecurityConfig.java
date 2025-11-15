6d759fa0fd14bbbe8b81dacaf30f975ae8df735f
chinhnd23 <chinhnd23@fpt.com>
2025-11-15 15:55:48 +0700

    chore(security): update CORS config to use allowedOriginPatterns for credentialed requests

--- src/main/java/com/fpt/careermate/config/SecurityConfig.java
+++ src/main/java/com/fpt/careermate/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -99,8 +100,9 @@ public class SecurityConfig {
 //                "file://*"  // Allow direct HTML file access for testing
 //        ));
         // Also allow null origin (for file:// protocol)
-        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedOrigin("*");
 
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setAllowCredentials(true);
